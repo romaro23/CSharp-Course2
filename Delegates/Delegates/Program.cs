1c4185df52326c499e00c11a84455814eb9d6d39
Nahorniy Roman <nahornyi.r22@fpm.dnu.edu.ua>
2024-12-14 14:13:38 +0200

    first commit

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,21 @@
+﻿using Delegates;
+
+class Program
+{
+    public static void OnCarEngineEvent(string msg)
+    {
+        Console.WriteLine("\n Message From Car Object ");
+        Console.WriteLine("=> {0}", msg);
+        Console.WriteLine("*************************\n");
+    }
+    static void Main(string[] args)
+    { // Cтворимо об'єкт Car
+        Car c1 = new Car("SlugBug", 100, 10);
+        c1.RegisterWithCarEngine(
+        new Car.CarEngineHandler(OnCarEngineEvent));
+        // Викличемо метод Accelerate (це ініціює події). 
+        Console.WriteLine("***** Speeding up *****");
+        for (int i = 0; i < 6; i++)
+            c1.Accelerate(20);
+    }
+}
